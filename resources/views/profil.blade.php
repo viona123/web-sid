8d23616792ba5a81fd671109d0bca92cb4a334b8
viona123 <syahputriviona@gmail.com>
2021-11-23 13:55:26 +0700

    Membuat tampilan profil penduduk

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon.png">
+    <title>Profil | {{ $penduduk->nama }}</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 m-auto shadow mt-4">
+                <div class="mb-5">
+                    <img src="/images/user.png" alt="ikon" class="m-auto d-block mb-4" style="width: 7rem">
+                    <h1 class="text-center">{{ $penduduk->nama }}</h1>
+                </div>
+                <hr>
+                <div class="mt-4">
+                    <div class="mb-3">
+                        <label class="form-label">Nomor Kartu Keluarga</label>
+                        <input type="number" class="form-control" value="{{ $penduduk->no_kk }}" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Nomor Identitas Kependudukan</label>
+                        <input type="number" class="form-control" value="{{ $penduduk->nik }}" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Tempat Tanggal Lahir</label>
+                        <input type="text" class="form-control" value="{{ $penduduk->tempat_lahir }}, {{ $penduduk->tanggal_lahir }}" disabled>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Tempat Tinggal</label>
+                        <input type="text" class="form-control" value="{{ $penduduk->tempat_tinggal }}" disabled>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
