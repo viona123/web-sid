383b1628f9b892ec3afd37164a5e4e961b253103
iwanharyatno <iwnharry61@gmail.com>
2021-12-30 18:20:01 +0700

    Memperbaiki tampilan
    Membuat menu bar pada halaman admin menjadi responsif terhadap ukuran
    layar, dan mengganti ukuran font root element menjadi 12px.

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -13,6 +13,7 @@
   <body>
     <nav class="navbar navbar-dark position-sticky top-0" style="background-color: #339DFF">
       <div class="container-fluid">
+		<i class="fas fa-bars text-white menu-bar-toggler fa-fw" onclick="toggleMenu(this)"></i>
         <a class="navbar-brand" href="#">
           <img src="/images/logo.jpeg" width="30" height="24" class="d-inline-block">
           Siska
@@ -25,36 +26,36 @@
 
     <div class="container-fluid">
       <div class="row">
-        <div class="col-md-2 pt-4 ps-0 bg-dark" style="height: 100vh">
+        <div class="col-md-2 pt-4 ps-0 bg-dark menu-bar" style="height: 100vh">
           <div class="list-group bg-dark">
             <h4 class="nav-header text-white ps-3">Menu Utama</h4>
             <hr class="bg-secondary">
-            <a class="nav-link text-white" href="/admin?desa={{ $desa->id }}"><i class="fas fa-home me-1"></i> Home</a>
-            <a class="nav-link text-white" href="/admin/identitas_desa?desa={{ $desa->id }}"><i class="fas fa-info me-1"></i> Info Desa</a>
+            <a class="nav-link text-white" href="/admin?desa={{ $desa->id }}"><i class="fas fa-home fa-fw me-1"></i> Home</a>
+            <a class="nav-link text-white" href="/admin/identitas_desa?desa={{ $desa->id }}"><i class="fas fa-info fa-fw me-1"></i> Info Desa</a>
             <div class="dropdown hihi">
               <button class="btn menu dropdown-toggle text-white" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-user"></i> Kependudukan
+                <i class="fas fa-user fa-fw me-1"></i> Kependudukan
               </button>
               <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                <li class="hihi"><a class="dropdown-item" href="/admin/penduduk?desa={{ $desa->id }}"><i class="fas fa-user"></i> Penduduk</a></li>
-                <li class="hihi"><a class="dropdown-item" href="/admin/keluarga?desa={{ $desa->id }}"><i class="fas fa-user-friends"></i> Keluarga</a></li>
-                <li class="hihi"><a class="dropdown-item" href="/admin/rumah-tangga?desa={{ $desa->id }}"><i class="fas fa-venus-mars"></i> Rumah tangga</a></li>
-                <li class="hihi"><a class="dropdown-item" href="/admin/kelompok?desa={{ $desa->id }}"><i class="fas fa-users"></i> Kelompok</a></li>
+                <li class="hihi"><a class="dropdown-item" href="/admin/penduduk?desa={{ $desa->id }}"><i class="fas fa-user fa-fw me-1"></i> Penduduk</a></li>
+                <li class="hihi"><a class="dropdown-item" href="/admin/keluarga?desa={{ $desa->id }}"><i class="fas fa-user-friends fa-fw me-1"></i> Keluarga</a></li>
+                <li class="hihi"><a class="dropdown-item" href="/admin/rumah-tangga?desa={{ $desa->id }}"><i class="fas fa-venus-mars fa-fw me-1"></i> Rumah tangga</a></li>
+                <li class="hihi"><a class="dropdown-item" href="/admin/kelompok?desa={{ $desa->id }}"><i class="fas fa-users fa-fw me-1"></i> Kelompok</a></li>
               </ul>
             </div>
-            <a class="nav-link text-white" href="#"><i class="fas fa-book-reader me-1"></i> Layanan Surat</a>
+            <a class="nav-link text-white" href="#"><i class="fas fa-book-reader fa-fw me-1"></i> Layanan Surat</a>
             <div class="dropdown hihi">
               <button class="btn menu dropdown-toggle text-white" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-clipboard"></i> Buku Administrasi
+                <i class="fas fa-clipboard fa-fw me-1"></i> Buku Administrasi
               </button>
               <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                <li class="hihi"><a class="dropdown-item" href="/admin/pengurus_desa?desa={{ $desa->id }}"><i class="fas fa-users"></i> Pengurus Desa</a></li>
+                <li class="hihi"><a class="dropdown-item" href="/admin/pengurus_desa?desa={{ $desa->id }}"><i class="fas fa-users fa-fw me-1"></i> Pengurus Desa</a></li>
               </ul>
             </div>
-            <a class="nav-link text-white" href="/admin/program-bantuan?desa={{ $desa->id }}"><i class="fas fa-coins me-1"></i> Program Bantuan</a>
-            <a class="nav-link text-white" href="#"><i class="fas fa-globe-asia me-1"></i> Pemetaaan</a>
-            <a class="nav-link text-white" href="#"><i class="far fa-envelope me-1"></i> SMS</a>
-            <a class="nav-link text-white" href="#"><i class="fas fa-cog me-1"></i> Pengaturan</a>
+            <a class="nav-link text-white" href="/admin/program-bantuan?desa={{ $desa->id }}"><i class="fas fa-coins fa-fw me-1"></i> Program Bantuan</a>
+            <a class="nav-link text-white" href="#"><i class="fas fa-globe-asia fa-fw me-1"></i> Pemetaaan</a>
+            <a class="nav-link text-white" href="#"><i class="far fa-envelope fa-fw me-1"></i> SMS</a>
+            <a class="nav-link text-white" href="#"><i class="fas fa-cog fa-fw me-1"></i> Pengaturan</a>
           </div>
         </div>
         <div class="col-md-10 pt-4" style="height: 100vh; overflow-y: scroll">
@@ -70,9 +71,26 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script>
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-      });
+		var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+			return new bootstrap.Tooltip(tooltipTriggerEl)
+		});
+
+		const menuBar = document.querySelector('.menu-bar');
+		let menuOpen = false;
+
+		function toggleMenu(toggler) {
+			if (!menuOpen) {
+				menuBar.style.display = "initial";
+				menuOpen = true;
+				toggler.classList.remove('fa-bars');
+				toggler.classList.add('fa-times');
+			} else {
+				menuBar.style.display = "none";
+				menuOpen = false;
+				toggler.classList.remove('fa-times');
+				toggler.classList.add('fa-bars');
+			}
+		}
     </script>
   </body>
 </html>
